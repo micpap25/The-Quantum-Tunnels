13b0f74056c07b05435367fa41d9dc3713f25aec
Michael Papadopoulos <micpap25@gmail.com>
2022-12-28 21:00:45 -0500

    Improve efficiency & readibility

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 # Load the main quantum circuit representing game logic (constant)
-import qiskit # pip3 install qiskit
-gameCircuit = qiskit.QuantumCircuit.from_qasm_file("game.qasm")
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, transpile # pip3 install qiskit
+gameCircuit = QuantumCircuit.from_qasm_file("game.qasm")
 
 # Load the mapping of states to outputs (constant)
 import json, gzip
@@ -10,6 +10,8 @@ outputData = json.loads(gzip.GzipFile("outputs.json.gz", 'r').read().decode("utf
 
 # Start in the default state 
 state = "0000000000000010011011"
+
+simulator = Aer.get_backend('qasm_simulator')
 
 # Game loop
 while True:
@@ -24,11 +26,14 @@ while True:
     classicalInput = input("              Command (try \"help\"): ")
 
     # Generate circuit combining user input and prev game state
-    inputCircuit = qiskit.QuantumCircuit(qiskit.QuantumRegister(3, "inpt"), qiskit.QuantumRegister(2, "plhp"), qiskit.QuantumRegister(2, "enhp"), qiskit.QuantumRegister(4, "evnt"), qiskit.QuantumRegister(3, "item"), qiskit.QuantumRegister(8, "ancl"), qiskit.ClassicalRegister(22, "cout"))
+    inputCircuit = QuantumCircuit(QuantumRegister(3, "inpt"), QuantumRegister(2, "plhp"), QuantumRegister(2, "enhp"), QuantumRegister(4, "evnt"), QuantumRegister(3, "item"), QuantumRegister(8, "ancl"), ClassicalRegister(22, "cout"))
     inputState = state[:-3] + {"h":"001","m":"011","a":"100","b":"101","f":"110","i":"111"}[classicalInput[0]]
     for i, char in enumerate(inputState[::-1]):
         if char == "1": inputCircuit.x(i)
 
+    circuit = inputCircuit.compose(gameCircuit)
+    compiled_circuit = transpile(circuit, simulator)
     # Combine and run the circuit (one shot)
-    state = list(qiskit.execute(inputCircuit.compose(gameCircuit), qiskit.Aer.get_backend('qasm_simulator'), shots=1, backend_options={"method": "matrix_product_state"}).result().get_counts().keys())[0][0:22]
+    output = simulator.run(compiled_circuit, shots=1)
+    state = list(output.result().get_counts().keys())[0][0:22]
 
