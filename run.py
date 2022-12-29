761fc0fa95ddc29d2e0faca8030190a4c8c33f73
Michael Papadopoulos <micpap25@gmail.com>
2022-12-28 21:51:14 -0500

    Clean up all the new lines

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ outputData = json.loads(gzip.GzipFile("outputs.json.gz", 'r').read().decode("utf
 # Dump all the screens
 with open("out.txt", "w", encoding="utf-8") as f:
     for key in outputData:
-        f.write(outputData[key])
+        f.write(outputData[key][-1864:])
